80019e8d1fd5b40ffdee53b4b317d97a4a7142a1
metalabyss <colorful.hoshi@gmail.com>
2015-12-19 01:13:11 +0600

    + конвертация в ОПН

--- lab-7/lab-7/calculator.c
+++ lab-7/lab-7/calculator.c
@@ -1,9 +1,6 @@
 ﻿#include <memory.h>
 #include "calculator.h"
 
-#define OPERATOR (1)
-#define NUMBER (0)
-
 int getNumber(char c)
 {
 	if (c >= '0' && c <= '9') return (int)(c - '0');
@@ -33,7 +30,8 @@ token_t *parse(unsigned char *string, int *tokensCount)
 	token_t *result = malloc(sizeof(token_t) * strlen(string));
 	*tokensCount = 0;
 
-	for (int i = 0; i < strlen(string); i++)
+	int i;
+	for (i = 0; i < strlen(string); i++)
 	{
 		if (getNumber(string[i]) == -1)
 		{
@@ -60,4 +58,73 @@ token_t *parse(unsigned char *string, int *tokensCount)
 	}
 
 	return result;
+}
+
+token_t *convertToRPN(token_t *expression, int expressionLength)
+{
+	token_t *result = malloc(sizeof(token_t)* expressionLength);
+	int resultLength = 0;
+
+	token_t *stack = malloc(sizeof(token_t)* expressionLength);
+	int stackEnd = -1; //стек пуст!
+
+	int i;
+	for (i = 0; i < expressionLength; i++)
+	{
+		//если встретилось число - кладём в массив
+		if (expression[i].type == NUMBER) { 
+			result[resultLength++] = expression[i];
+		}
+		//если встретилась откр. скобка - помещаем её в стек.
+		else if (expression[i].type == OPERATOR && expression[i].symbol == '(')
+		{
+			stack[++stackEnd] = expression[i];
+		}
+		else if (expression[i].type == OPERATOR && expression[i].symbol == ')')
+		{
+			while (stackEnd >= 0)
+			{
+				//если наткнулись на откр. скобку - выходим из цикла
+				if (stack[stackEnd].type == OPERATOR && stack[stackEnd].symbol == '(') break;
+				result[resultLength++] = stack[stackEnd--];
+			}
+
+			//если так и не встретили закр. скобку - в выражении ошибка
+			if (stackEnd == -1) return NULL;
+
+			//удаляем открывающую скобку
+			stackEnd--;
+		}
+		else
+		{
+			//смотрим приоритеты операторов. Если стек непуст, и у текущего оператора приоритет меньше, то...
+			int currOperatorPriority = getOperatorPriority(expression[i].symbol);
+			while (stackEnd >= 0 && currOperatorPriority <= getOperatorPriority(stack[stackEnd].symbol))
+			{
+				result[resultLength] = stack[stackEnd]; //помещаем его в конец выходной строки
+				resultLength++;
+				stackEnd--;
+			}
+
+			stack[++stackEnd] = expression[i];
+		}
+	}
+
+	while (stackEnd >= 0)
+	{
+		if (stack[stackEnd].type == OPERATOR && stack[stackEnd].symbol == '(') return NULL; //неправильно расставлены скобки!
+		result[resultLength++] = stack[stackEnd--];
+	}
+
+	free(stack);
+	return result;
+}
+
+char *calculate(token_t *expression, int expressionLength)
+{
+	token_t *rpn = convertToRPN(expression, expressionLength);
+	if (rpn == NULL) return "syntax error";
+
+
+	
 }
--- lab-7/lab-7/calculator.h
+++ lab-7/lab-7/calculator.h
@@ -1,5 +1,8 @@
 ﻿#include "token.h"
 
+#define OPERATOR (1)
+#define NUMBER (0)
+
 int getNumber(char c);
 
 int getOperatorPriority(char c);
@@ -7,15 +10,19 @@ int getOperatorPriority(char c);
 /* Фунция разбивает строку на операторы и операнды:
 	string - введенная строка,
 	tokensCount - указатель на кол-во токенов.
-	Возвращаемое значение - массив (список?) из токенов.
+	Возвращаемое значение - массив из токенов.
 	Если в выражении встретился недопустимый символ - возвращается нулевой указатель. */
 token_t *parse(unsigned char *string, int *tokensCount);
 
 /* Функция представляет выражение в обратной польской нотации (reverse polish notation):
-	expression - список из токенов
-	expressionLength - длина этого списка (выражения)
-	Возвращаемое значение - список (выражение) в ОПН.
+	expression - массив из токенов
+	expressionLength - длина этого массива (выражения)
+	Возвращаемое значение - выражение в ОПН (массив).
 	Если в выражении ошибка - возвращается нулевой указатель. */
 token_t *convertToRPN(token_t *expression, int expressionLength);
 
-token_t *calculate();
+/* Функция вычисляет выражение, записанное в ОПН.
+	expression - массив из токенов,
+	expressionLength - длина выражения,
+	Возвращаемое значение - строка с результатом вычисления, либо "syntax error/division by zero" в случае ошибки. */
+char *calculate(token_t *expression, int expressionLength);
